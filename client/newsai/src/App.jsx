6b529b1dec769b87a74ea4a574443b979e532db6
palak <palakchoudhary1842001@gmail.com>
2025-03-11 13:05:20 +0530

    updated till login page

--- client/newsai/src/App.jsx
+++ client/newsai/src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-
-
+import '@mantine/core/styles.css';
+import {Routes , Route} from "react-router-dom"
+import Login  from '../Pages/Login';
 function App() {
   
 
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
     </div>
   )
 }
